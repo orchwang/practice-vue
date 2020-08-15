d927a66fb9bc074a438a9a5b090e4b66e2be8f0b
orchwang <hwang.jongtaek@gmail.com>
2020-08-15 16:39:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<p align="center">
+  <br>
+  <img width="200" src="./src/assets/logo/orchwang.png" alt="logo of orchwang">
+  <br>
+  <br>
+</p>
+
+# Intro
+
+Vue 연습을 위한 저장소. 다양한 형태의 웹 어플리케이션 기능을 Vue 로 구현해 보는 것을 목표로 하오크 
+
+# Roadmap
+
+- Element UI 적용
+- 다양한 디자인패턴 연습
+- 대시보드 시스템 구현
+- Vue Routinng System 구현
+- 사용자 인증을 포함하는 백엔드와의 연동
+- CRUD (Todo, Board 등)
+
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="OrcHwang logo" src="./assets/logo/orchwang.png">
+    <HelloWorld msg="OrcHwang's Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#app img {
+  width: 400px;
+}
+</style>
