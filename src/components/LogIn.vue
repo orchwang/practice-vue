4fe9cca82b09d99cb64b09d349e1a478dff6557a
orchwang <hwang.jongtaek@gmail.com>
2020-08-16 20:47:16 +0900

    Add Router

---
+++ src/components/LogIn.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hello">
+    <h1>LogIn</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+  props: {}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+
+import LogIn from './components/LogIn.vue'
 
 Vue.config.productionTip = false
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'LogIn',
+      component: LogIn
+    },
+  ]
+})
 
 new Vue({
   render: h => h(App),
