0efa4558008ec8e9820a3773204d77c45696877b
orchwang <hwang.jongtaek@gmail.com>
2020-08-17 16:57:12 +0900

    Remove font files Update scss font settings Update router and components

---
+++ src/components/dashboard/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>Statistics</h1>
+    <p>본 앱은 Orc Hwang 이 Vue 를 학습하기 위한 앱 입니다.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Statistics extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="home">
+  <div class="dashboard">
     <img alt="Vue logo" src="../assets/logo/orchwang.png">
+    <Statistics />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Statistics from '@/components/dashboard/Statistics.vue';
 
 @Component({
   components: {
+    Statistics,
   },
 })
 export default class Home extends Vue {}
